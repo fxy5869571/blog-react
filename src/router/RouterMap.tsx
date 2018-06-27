fef53eb0a40f026a6005e8d8844cd2db549e6bb1
yqy <83014588@qq.com>
2018-06-27 17:25:53 +0800

    添加代码按路由分割功能

--- src/router/RouterMap.tsx
+++ src/router/RouterMap.tsx
@@ -1,19 +1,45 @@
 import * as React from 'react'
+import * as Loadable from 'react-loadable'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import App from '../containers/App'
-import Article from '../containers/Article'
-import Articles from '../containers/Articles'
-import Resume from '../containers/Resume'
-import TimeFile from '../containers/TimeFile'
-
+import loading from './loading'
+const RouterList: any[] = [
+  {
+    component: Loadable({
+      loader: () => import('../containers/Articles'),
+      loading
+    }),
+    path: '/'
+  },
+  {
+    component: Loadable({
+      loader: () => import('../containers/Article'),
+      loading
+    }),
+    path: '/article/:Id'
+  },
+  {
+    component: Loadable({
+      loader: () => import('../containers/Resume'),
+      loading
+    }),
+    path: '/resume'
+  },
+  {
+    component: Loadable({
+      loader: () => import('../containers/TimeFile'),
+      loading
+    }),
+    path: '/time-file'
+  }
+]
 const RouterMap = () => (
   <Router>
     <App>
       <Switch>
-        <Route exact={true} path="/" component={Articles} />
-        <Route exact={true} path="/article/:Id" component={Article} />
-        <Route exact={true} path="/time-file" component={TimeFile} />
-        <Route exact={true} path="/resume" component={Resume} />
+        {RouterList.map(item => (
+          <Route exact={true} path={item.path} component={item.component} />
+        ))}
       </Switch>
     </App>
   </Router>
