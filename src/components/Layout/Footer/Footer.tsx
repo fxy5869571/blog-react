c543467e5a3b6b192ea5e995c7e10ee3d5713c43
yqy <83014588@qq.com>
2018-07-18 18:00:14 +0800

    删除别人的备案信息(⊙o⊙)…

--- src/components/Layout/Footer/Footer.tsx
+++ src/components/Layout/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import './style.less'
 const Footer = () => (
   <div className="footer">
     <div className="item">All Rights Reserved</div>
-    <div className="item">蜀ICP备15021330号 Copyright © 2015 - 2017</div>
+    <div className="item">感谢开源世界提供的技术支持</div>
   </div>
 )
 
