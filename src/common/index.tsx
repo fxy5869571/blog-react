089c856c96f4823709eaa66ec7a17b6ec55b5026
fxy5869571 <996276475@qq.com>
2018-08-01 22:29:15 +0800

    嘻嘻现在本地代码可以直接访问服务器接口啦，大佬们别删我文章呀呀呀呀

--- src/common/index.tsx
+++ src/common/index.tsx
@@ -1,5 +1,6 @@
 import 'whatwg-fetch'
-export const api = 'http://localhost:8001/api'
+// export const api = 'http://localhost:8001/api'
+export const api = 'http://132.232.34.190/api'
 export const headerImg =
   'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1527245834290&di=2286cc7354adc925f79b92b719162dd4&imgtype=0&src=http%3A%2F%2Fpic32.photophoto.cn%2F20140828%2F0005018403917054_b.jpg'
 export const format = (date: string): string => {
